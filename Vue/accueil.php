98f41dbd7ca780dbd4c4c7bd9bd6b9fef687ca18
BomberQLF <tmurphyparis@gmail.com>
2024-11-18 15:05:15 +0100

    Update accueil.php

---
+++ Vue/accueil.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Style/navbar.css">
+    <title>Accueil - ENT</title>
+</head>
+
+<body>
+    <nav class="navbar">
+        <ul>
+            <li class="left"><a href="#">Home</a></li>
+            <div class="center">
+                <li class="has-submenu">
+                    <a href="#">Mon suivi</a>
+                    <ul class="submenu">
+                        <li><a href="#">Notes</a></li>
+                        <li><a href="#">Évaluation</a></li>
+                        <li><a href="#">To do list</a></li>
+                        <li><a href="#">Absences et retards</a></li>
+                    </ul>
+                </li>
+                <li class="has-submenu">
+                    <a href="#">Planning et réservation</a>
+                    <ul class="submenu">
+                        <li><a href="#">Emploi du temps</a></li>
+                        <li><a href="#">Salles et Matériels</a></li>
+                    </ul>
+                </li>
+                <li class="has-submenu">
+                    <a href="#">Vie étudiante</a>
+                    <ul class="submenu">
+                        <li><a href="#">Crous et mon IZLY</a></li>
+                        <li><a href="#">Événements</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Messagerie</a></li>
+            </div>
+            <li class="right">
+                <a href="#">Bonjour, Tom <img src="../image/uploads/photo_default.png" alt=""></a>
+            </li>
+        </ul>
+    </nav>
+</body>
+
+</html>
