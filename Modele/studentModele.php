03d00ff7190306e0cbedb37da03eb621260bceb1
BomberQLF <tmurphyparis@gmail.com>
2024-11-18 08:48:12 +0100

    Suppression fonction inscription

--- Modele/studentModele.php
+++ Modele/studentModele.php
@@ -5,38 +5,6 @@ function connect_db()
     return $db;
 }
 
-function inscription($nom, $prenom, $login, $motdepasse, $motdepasse2)
-{
-    // On regarde si les mots de passe correspondent bien
-    if ($motdepasse !== $motdepasse2) {
-        return "Les mots de passe ne correspondent pas";
-    }
-    $db = connect_db();
-    $stmt = $db->prepare("SELECT * FROM utilisateurs WHERE login = :login");
-    $stmt->bindParam(':login', $login, PDO::PARAM_STR);
-    $stmt->execute();
-
-    // On regarde si le login est déjà pris
-    if ($stmt->fetch(PDO::FETCH_ASSOC)) {
-        return "Login déjà pris";
-    } else {
-        $requete = "INSERT INTO utilisateurs VALUES (NULL, :login, :nom, :prenom, :motdepasse, :photo, 0)";
-        $stmt = $db->prepare($requete);
-
-        // on met l'url de la photo par défaut
-        $photo = "images/uploads/profil_default.png";
-        $hash = password_hash($motdepasse, PASSWORD_DEFAULT);
-
-        $stmt->bindParam(':login', $login, PDO::PARAM_STR);
-        $stmt->bindParam(':nom', $nom, PDO::PARAM_STR);
-        $stmt->bindParam(':prenom', $prenom, PDO::PARAM_STR);
-        $stmt->bindParam(':motdepasse', $hash, PDO::PARAM_STR);
-        $stmt->bindParam(':photo', $photo, PDO::PARAM_STR);
-        $stmt->execute();
-        return "Inscription réussie. Vous pouvez vous connecter";
-    }
-}
-
 function handleLogin($tab)
 {
     $pdo = connect_db();
