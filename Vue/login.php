9b2092cede4f1d38f974dc28f754164300994be2
BomberQLF <tmurphyparis@gmail.com>
2024-11-18 09:36:19 +0100

    Modification architecture HTML

--- Vue/login.php
+++ Vue/login.php
@@ -1,20 +1,39 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <link rel="stylesheet" href="./Style/login.css">
+    <title>Login - ENT</title>
 </head>
-<body>
-    <form action="index.php?action=login">
-        <label for="login">Login</label><br>
-        <input type="text" name="login" class="login"><br>
 
-        <label for="motdepasse">Mot de passe</label><br>
-        <input type="password" name="motdepasse" class="motdepasse">
-        <input type="submit">
+<body class="login-body">
+    <div class="login-container">
+        <div class="choice-login-container">
+            <h1>Bienvenue sur l'ENT</h1>
+            <p>Veuillez choisir votre statut</p>
+        </div>
+        <div class="form-login-container">
+            <form action="/ENT/index.php?action=login" method="POST">
+                <h2 id="connecter-login">Se connecter</h2>
 
-        <?php if(isset($error)) { echo $error; } ?>
-    </form>
+                <label for="login">Login</label>
+                <input type="text" name="login" class="login" placeholder="Tapez votre login">
+                <div class="mdp-container">
+                    <label for="motdepasse">Mot de passe</label>
+                    <input type="password" name="motdepasse" class="motdepasse" placeholder="Tapez votre mot de passe">
+                </div>
+
+                <a href="#">Mot de passe oublié ?</a>
+                <input type="submit" value="Se connecter">
+
+                <?php if (isset($error)) {
+                    echo $error;
+                } ?>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
