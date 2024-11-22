9898d0a0a5066b80db17bac612ba5d18559c58d0
Nicolas24700 <150784671+Nicolas24700@users.noreply.github.com>
2024-11-22 17:14:51 +0100

    Update accueil.php

--- Vue/accueil.php
+++ Vue/accueil.php
@@ -5,44 +5,83 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Style/navbar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" />
     <title>Accueil - ENT</title>
 </head>
 
 <body>
-    <nav class="navbar">
-        <ul>
-            <li class="left"><a href="#" id="home">Home</a></li>
-            <div class="center">
+    <nav>
+        <a href="#" class="navbar-home"><i class="fa-solid fa-house"></i></a>
+        <button class="hamburger" id="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+
+        <!-- Barre de navigation d'origine -->
+
+        <ul id="navbarLinks">
+            <li><a href="#" class="navbar-item"><i class="fa-solid fa-book"></i>Mon suivi</a>
+                <ul class="submenu">
+                    <li><a href="#">Notes</a></li>
+                    <li><a href="#">To do list</a></li>
+                    <li><a href="#">Absences et retards</a></li>
+                </ul>
+            </li>
+            <li><a href="#" class="navbar-item"><i class="fa-solid fa-calendar-days"></i>Planning et réservation</a>
+                <ul class="submenu">
+                    <li><a href="#">Emploi du temps</a></li>
+                    <li><a href="#">Réservation salles et matériels</a></li>
+                </ul>
+            </li>
+            <li><a href="#" class="navbar-item"><i class="fa-solid fa-graduation-cap"></i>Vie étudiante</a>
+                <ul class="submenu">
+                    <li><a href="#">Crous et mon IZLY</a></li>
+                    <li><a href="#">Événements</a></li>
+                </ul>
+            </li>
+            <li><a href="#" class="navbar-item"><i class="fa-solid fa-comment"></i>Messagerie</a></li>
+        </ul>
+
+
+        <a href="#" class="navbar-profile">
+            <span>Bienvenue, Anastasia</span>
+            <div class="profile-circle"></div>
+        </a>
+
+        <!-- Barre de navigation version téléphone -->
+
+        <div class="side-menu" id="sideMenu">
+            <ul>
+                <li><a href="#"><i class="fa-solid fa-house"></i>Accueil</a></li>
                 <li class="has-submenu">
-                    <a href="#">Mon suivi</a>
+                    <a href=""><i class="fa-solid fa-book"></i>Mon suivi</a>
                     <ul class="submenu">
-                        <li><a href="#" id="notes">Notes</a></li>
-                        <li><a href="#" id="evaluation">Évaluation</a></li>
-                        <li><a href="#" id="todolist">To do list</a></li>
-                        <li><a href="#" id="absenceRetards">Absences et retards</a></li>
+                        <li><a href="#">Notes</a></li>
+                        <li><a href="#">To do list</a></li>
+                        <li><a href="#">Absences et retards</a></li>
                     </ul>
                 </li>
                 <li class="has-submenu">
-                    <a href="#">Planning et réservation</a>
+                    <a href=""><i class="fa-solid fa-calendar-days"></i>Planning et réservation</a>
                     <ul class="submenu">
                         <li><a href="#">Emploi du temps</a></li>
-                        <li><a href="#">Salles et Matériels</a></li>
+                        <li><a href="#">Réservation salles et matériels</a></li>
                     </ul>
                 </li>
                 <li class="has-submenu">
-                    <a href="#">Vie étudiante</a>
+                    <a href=""><i class="fa-solid fa-graduation-cap"></i>Vie étudiante</a>
                     <ul class="submenu">
                         <li><a href="#">Crous et mon IZLY</a></li>
                         <li><a href="#">Événements</a></li>
                     </ul>
                 </li>
-                <li><a href="#">Messagerie</a></li>
-            </div>
-            <li class="right">
-                <a href="#">Bonjour, Tom <img src="../image/uploads/photo_default.png" alt=""></a>
-            </li>
-        </ul>
+                <li><a href="#"><i class="fa-solid fa-comment"></i>Messagerie</a></li>
+            </ul>
+        </div>
+
     </nav>
+    <script src="../index.js"></script>
 </body>
 
 </html>
