bc2c421d9a5813bee89a3fbc4306406298ba2282
BomberQLF <tmurphyparis@gmail.com>
2024-11-26 23:37:57 +0100

    Update studentModele.php

--- Modele/studentModele.php
+++ Modele/studentModele.php
@@ -1,14 +1,14 @@
 <?php
 function connect_db()
 {
-    $db = new PDO('mysql:host=localhost;dbname=miniprojetphp;port=3306;charset=utf8','root','');
+    $db = new PDO('mysql:host=localhost;dbname=ent;port=8888', 'root', 'root');
     return $db;
 }
 
 function handleLogin($tab)
 {
     $pdo = connect_db();
-    $query = $pdo->prepare("SELECT id_utilisateurs, login, motdepasse, prenom, nom, photo FROM utilisateurs WHERE login = :login");
+    $query = $pdo->prepare("SELECT * FROM utilisateurs WHERE login = :login");
 
     $query->bindParam(':login', $tab['login'], PDO::PARAM_STR);
     $query->execute();
@@ -18,11 +18,13 @@ function handleLogin($tab)
     // Vérifier si un utilisateur est trouvé et comparer les mdp
     if ($user && password_verify($tab['mot_de_passe'], $user['mot_de_passe'])) {
         // Déclaration des variables de sessions utiles pour la suite
-        $_SESSION['user'] = $user['login'];
+        $_SESSION['login'] = $user['login'];
         $_SESSION['prenom'] = $user['prenom'];
         $_SESSION['nom'] = $user['nom'];
-        $_SESSION['user_id'] = $user['id_utilisateurs'];
-        $_SESSION['photo'] = $user['photo'];
+        $_SESSION['id_utilisateur'] = $user['id_utilisateur'];
+        $_SESSION['photo'] = $user['photo_profil'];
+        $_SESSION['tp'] = $user['tp'];
+        $_SESSION['role'] = $user['admin'];
 
         return true;
     } else {
